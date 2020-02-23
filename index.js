2b37bb5abbb5f732803be1d4b785f54e75eff69b
Bobby Boyd <jeffbob@gmail.com>
2020-02-22 22:20:44 -0700

    added console log msg for testin

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const Discord = require('discord.js');
 const tokken = require('./tk.js');
-//const bot = Discord.Client();
+const bot = new Discord.Client();
+const token = tokken.getKey();
+//console.log(tokken.getKey());
+//console.log(tokken.getId());
+//console.log(tokken.getName());
 
-console.log(tokken.getKey());
-console.log(tokken.getId());
-console.log(tokken.getName());
+bot.on('ready', () => {
+	console.log('This Bot Exists');
+})
 
+bot.on('message', msg => {
+	console.log("Recieved Message: " + msg.content);
+	if(msg.content === "HELLO"){
+		console.log("Reply Message: " + "HELLO SPACEMONKEY!");
+		msg.reply("HELLO SPACEMONKEY!");
+	}
+})
+
+
+bot.login(token);
