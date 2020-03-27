944d3216a4bcb24cd131e6a250cc6cc48d00fc0b
Bobby Boyd <jeffbob@gmail.com>
2020-03-27 15:40:20 -0600

    added prefix to tk.js and fuction

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const Discord = require('discord.js');
-const tokken = require('./tk.js');
+const Discord = require("discord.js");
+const config = require("./tk.js");
+//const config = require("./config.json");
 const bot = new Discord.Client();
-const token = tokken.getKey();
+const token = config.getKey();
 //console.log(tokken.getKey());
 //console.log(tokken.getId());
 //console.log(tokken.getName());
+console.log("the current prefix is " + config.getPrefix());
 
-bot.on('ready', () => {
-	console.log('This Bot Exists');
-})
+bot.on("ready", () => {
+  console.log("This Bot Exists");
+});
 
-bot.on('message', msg => {
-	console.log("Recieved Message: " + msg.content);
-	if(msg.content === "HELLO"){
-		console.log("Reply Message: " + "HELLO SPACEMONKEY!");
-		msg.reply("HELLO SPACEMONKEY!");
-	}
-})
-
+bot.on("message", (msg) => {
+  console.log("Recieved Message: " + msg.content);
+  if (msg.content === "HELLO") {
+    console.log("Reply Message: " + "HELLO SPACEMONKEY!");
+    msg.reply("HELLO SPACEMONKEY!");
+  }
+});
 
 bot.login(token);
